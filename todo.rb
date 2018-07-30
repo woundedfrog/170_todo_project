be681910bc22dbf2d33b072df3536a7fc4cc54cc
Bernard Maisel <ntjust26@gmail.com>
2018-07-30 16:38:50 +0900

    added more changes

--- todo.rb
+++ todo.rb
@@ -15,15 +15,21 @@ get "/" do
   redirect "/lists"
 end
 
+
+
+
+# View all lists
 get "/lists" do
   @lists = session[:lists]
   erb :lists, layout: :layout
 end
 
+# Render the new list form
 get "/lists/new" do
   erb :new_list, layout: :layout
 end
 
+# Create a new list
 post "/lists" do
   session[:lists] << { name: params[:list_name], todos: [] }
   redirect "/lists"
