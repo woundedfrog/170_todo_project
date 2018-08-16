4eb88e2f33718da6e56b053628a23dfce4861f36
Bernard Maisel <ntjust26@gmail.com>
2018-08-16 14:15:21 +0900

    Added validation
    Added code for validating new lists

--- todo.rb
+++ todo.rb
@@ -31,7 +31,13 @@ end
 
 # Create a new list
 post "/lists" do
-  session[:lists] << { name: params[:list_name], todos: [] }
-  session[:success] = "The list has been created."
-  redirect "/lists"
+  list_name = params[:list_name].strip
+  if list_name.size >= 1 && list_name.size <= 100
+    session[:lists] << {name: list_name, todos: []}
+    session[:success] = "The list has been created."
+    redirect "/lists"
+  else
+    session[:error] = "List name must be between 1 and 100 characters."
+    erb :new_list, layout: :layout
+  end
 end
