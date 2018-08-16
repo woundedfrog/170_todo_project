8c1e339a5f26ebd4ca5015a39799cac4f24b3cc6
Bernard Maisel <ntjust26@gmail.com>
2018-08-16 14:18:13 +0900

    refactored code into layout.erb

--- todo.rb
+++ todo.rb
@@ -32,7 +32,7 @@ end
 # Create a new list
 post "/lists" do
   list_name = params[:list_name].strip
-  if list_name.size >= 1 && list_name.size <= 100
+  if (1..100).cover? list_name.size
     session[:lists] << {name: list_name, todos: []}
     session[:success] = "The list has been created."
     redirect "/lists"
