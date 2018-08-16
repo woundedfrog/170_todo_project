99b3e86d410f3f8b7b504c0776ab8fa7c6dd3523
Bernard Maisel <ntjust26@gmail.com>
2018-08-16 16:39:13 +0900

    added functionality
    Can add todo items to a list

--- todo.rb
+++ todo.rb
@@ -36,6 +36,13 @@ def error_for_list_name(name)
   end
 end
 
+# Return an error message if the name is invalid. Return nil if name is valid.
+def error_for_todo(name)
+  if !(1..100).cover? name.size
+    "Todo must be between 1 and 100 characters."
+  end
+end
+
 # Create a new list
 post "/lists" do
   list_name = params[:list_name].strip
@@ -52,8 +59,8 @@ post "/lists" do
 end
 
 get "/lists/:id" do
-  id = params[:id].to_i
-  @list = session[:lists][id]
+  @list_id = params[:id].to_i
+  @list = session[:lists][@list_id]
   erb :list, layout: :layout
 end
 
@@ -87,4 +94,21 @@ post "/lists/:id/destroy" do
   session[:lists].delete_at(id)
   session[:success] = "The list has been deleted."
   redirect "/lists"
+end
+
+# Adding new todos to a list
+post "/lists/:list_id/todos" do
+  @list_id = params[:list_id].to_i
+  @list = session[:lists][@list_id]
+  text = params[:todo].strip
+
+  error = error_for_todo(text)
+  if error
+    session[:error] = error
+    erb :list, layout: :layout
+  else
+    @list[:todos] << { name: text, completed: false,}
+    session[:success] = "The todo was added."
+    redirect "/lists/#{@list_id}"
+  end
 end
