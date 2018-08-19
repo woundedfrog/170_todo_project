313d2812fba6039c389c6ae9180b4a959ab306c1
Bernard Maisel <ntjust26@gmail.com>
2018-08-19 17:41:09 +0900

    visual stule for todo check marks

--- todo.rb
+++ todo.rb
@@ -120,6 +120,19 @@ post "/lists/:list_id/todos/:id/destroy" do
 
   todo_id = params[:id].to_i
   @list[:todos].delete_at(todo_id)
-  session[:success] = "The todo have been deleted."
+  session[:success] = "The todo has been deleted."
+  redirect "/lists/#{@list_id}"
+end
+
+# Update the status of a todo
+
+post "/lists/:list_id/todos/:id" do
+  @list_id = params[:list_id].to_i
+  @list = session[:lists][@list_id]
+
+  todo_id = params[:id].to_i
+  is_completed = params[:completed] == "true"
+  @list[:todos][todo_id][:completed] = is_completed
+  session[:success] = "The todo has been updated."
   redirect "/lists/#{@list_id}"
 end
